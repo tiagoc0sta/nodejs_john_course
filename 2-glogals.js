8eab369de1ccdab8793c9a56d328740d44b34b6a
Tiago Costa <tiagodpv@gmail.com>
2022-05-01 20:23:25 -0400

    first commit

---
+++ 2-glogals.js
@@ -0,0 +1,12 @@
+// GLOBALS  - NO WINDOWS !!!!
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (CommonJS)
+// module     - info about current module (file)
+// process    - info about env where the program is being executed
+
+setInterval(() =>{
+  console.log('hello world')
+}, 1000);
+
---
+++ 4-names.js
@@ -0,0 +1,11 @@
+//local 
+const secret = 'SUPER SECRET'
+
+// share
+const john = 'jhon'
+const peter = 'peter'
+
+
+
+
+module.exports =  {john, peter}
